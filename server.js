6057148ab7c5fc1a5d6155c42725c0c02710ad41
Tyler Everett <tylertechnotrade@gmail.com>
2021-10-15 17:27:07 -0700

    4th commit

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET Route for homepage
-// ///app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -28,24 +28,24 @@ app.get("/notes", (req, res) => {
 
 app.get("/api/notes", (req, res) => {
     res.json(database)
- })
+})
 
 
-app.post("/api/notes", (req, res) =>{
+app.post("/api/notes", (req, res) => {
     var noteDetails = req.body
     noteDetails.id = uniqid()
     database.push(noteDetails)
-fs.writeFileSync(path.join(__dirname, '/db/db.json'),JSON.stringify(database))
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(database))
     res.json()
-} )
+})
 
 app.delete("/api/notes/:id", (req, res) => {
-    var id = req.params.id 
+    var id = req.params.id
     var filterDb = database.filter(note => {
         return note.id !== id
     })
 
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'),JSON.stringify(filterDb))
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(filterDb))
     database = filterDb
     res.json()
 })
